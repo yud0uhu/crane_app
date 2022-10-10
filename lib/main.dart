83da5dd88aa4d5d7cc8d04b6616e1f47c3bf7278
yud0uhu <cist.b219.y.osaka@gmail.com>
2022-10-11 02:40:12 +0900

    feat: initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:crane_app/header.dart';
+import 'package:flutter/material.dart';
+import 'package:crane_app/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primaryColor: Color.fromRGBO(93, 16, 73, 1),
+          primarySwatch: Colors.grey),
+      home: Scaffold(
+          body: Stack(
+        children: <Widget>[
+          Header(),
+          Container(padding: const EdgeInsets.only(top: 370), child: HomePage())
+        ],
+      )),
+    );
+  }
+}
